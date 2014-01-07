ff22e4a619bd0dc9ac55ea9bdbc04dcb6b86394b
Apprentice <apprentice@DBC-Infinity-3.local>
2014-01-07 15:20:51 -0800

    Adds solutions for each method

--- source/power_of_enumerables.rb
+++ source/power_of_enumerables.rb
@@ -1,32 +1,42 @@
 # TODO: Print the elements at indices 1, 3, 5, 7, etc. on separate lines.
 #       You should make use of Enumerable#each_with_index
 def print_odd_indexed_integers(array)
+  array.each_with_index { |element, index| puts element if index % 2 != 0 }
 end
 
 # TODO: Return the odd numbers from a list of integers.
 #       You should make use of Enumerable#select
 def odd_integers(array)
+  array.select { |num| num % 2 != 0 }
 end
 
 # TODO: Return the first number from an Array that is less than a particular number - 'limit.'
 #       You should make use of Enumerable#find
 def first_under(array, limit)
+  array.find { |num| num < limit }
 end
 
 # TODO: Take an Array of Strings and return a new Array with an exclamation point appended to each String.
 #       You should make use of Enumerable#map
 def add_bang(array)
+  array.map {|element| element.to_s + "!" }
 end
 
 # TODO: Calculate the sum of an Array of numbers.
 #       You should make use of Enumerable#reduce
 def sum(array)
+  array.reduce(:+)
 end
 
 # TODO: Reorganize an Array of the elements into groups of 3, and then sort each group alphabetically.
 #       You should make use of Enumerable#each_slice
 def sorted_triples(array)
+  sorted = []
+  array.each_slice(3) { |sub_array| sorted << sub_array.sort }
+  sorted
 end
+
+
 
 # Driver code... don't change this.
 print_odd_indexed_integers([2, 4, 6, 8, 10, 12]) # this should print "4\n8\n12"
